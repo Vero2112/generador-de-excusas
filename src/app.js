b634fd21650ff1a104f099d05dc5a73188f35775
Vero2112 <verorodriguezruiz21@gmail.com>
2022-05-04 18:22:36 +0000

    añadir ejercicio acabado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let numeroaleatorio = Math.random() * 10;
+  document.querySelector("#Btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generadordeexcusas();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generadordeexcusas = function() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoindex = Math.floor(Math.random() * who.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+  return who[whoindex] + " " + what[whatindex] + " " + when[whenindex];
 };
